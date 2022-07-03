448ba9eb12fef5bfba882c5831cc693315d47f28
slee <slee@openware.com>
2022-07-03 20:25:29 +0700

    a

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,14 @@ const transfer = require('./transfer');
 const setRoutes = (app) => {
 	app.use('/api/v1/account', account);
 	app.use('/api/v1/trade', transfer);
-	app.use((req, res, next) => {
-		res.status(404).json({
-			error: {
-				code: 'PAGE_NOT_FOUND',
-				message: 'Requested page is not found'
-			}
-		});
-	});
+	// app.use((req, res, next) => {
+	// 	res.status(404).json({
+	// 		error: {
+	// 			code: 'PAGE_NOT_FOUND',
+	// 			message: 'Requested page is not found'
+	// 		}
+	// 	});
+	// });
 };
 
 module.exports = {
